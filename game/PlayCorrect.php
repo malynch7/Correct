31161bb26b1f5bd52ca3d350bbb427b133e2a923
Mark Lynch <malynch7@gmail.com>
2018-10-12 14:51:15 -0400

    roughly implemented deck creation and gameboard serialization

---
+++ game/PlayCorrect.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>How to Play</title>
+    <link rel="stylesheet" type="text/css" href="PlayCorrect.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheetproject1.css">
+</head>
+
+<body>
+
+<table id="mainframe">
+    <tr>
+        <td>
+            <ul>
+                <li><a class="active" href="../Correct.html">Home</a></li>
+                <li><a href="">Play</a></li>
+                <li><a href="../howtoplay.html">How to play</a></li>
+                <li><a href="../youtube.html">YouTube Video</a>
+                <li><a href="../aboutcontributors.html">About Contributors</a></li>
+                <li class="liclass"> <a href="https://github.com/malynch7/Web-Programming-Project-1">GitHub Repository</a></li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td><h1>Correct!</h1></td>
+    </tr>
+    <?php
+
+        $numberOfCards = 16;
+        $gridSize = 4;
+
+        class Card{
+
+            var $face;
+            var $match;
+            var $flipped;
+            var $matched;
+
+            function __construct($face,$match){
+                $this->face = $face;
+                $this->match = $match;
+                $this->flipped = 0;
+                $this->matched = 0;
+            }
+            function toString(){
+                return $this->face . $this->match . $this->flipped . $this->matched ;
+            }
+        }
+
+        if (!isset($_REQUEST["cards"])){
+
+            //create card deck
+            $deck = array();
+            $letter;
+            for($i = 0; $i < $numberOfCards; $i++){
+                if(($i % 2) == 0){
+                    $letter = "A";
+                }else{
+                    $letter = "B";
+                }
+                array_push($deck, new Card("../images/GameDesign/card" . ($i / 2) . $letter . ".png", ($i / 2)));
+            }
+            shuffle($deck);
+
+            //present new game button
+            echo "<tr><td>
+                    <br><br>
+                    <form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
+                        <input type=\"hidden\" name=\"cards\" value=\"" .serialize($deck) . "\">
+                        <input type=\"hidden\" name=\"start\" value=\"true\">
+                        <input type=\"submit\" value=\"Play Now!\">
+                    </form>
+                   </td></tr>
+                ";
+
+        }else{
+
+            //gameplay loop
+            $deck = unserialize($_REQUEST["cards"]);
+
+            echo "<tr><td>
+                    <br><br>
+                        <table class='center'>";
+            for($i = 0; $i < $gridSize; $i++){
+                echo "<tr>";
+                for($j = 0; $j < $gridSize; $j++){
+                    if($deck[$i + $j]->flipped == 1){
+                        echo "<td><img src='" . $deck[$i + $j]->face ."' class='center'></td>";
+                    }else{
+                        echo "<td><img src='../images/GameDesign/backside_of_card.png' class='center'></td>";
+                    }
+
+                }
+                echo "</tr>";
+            }
+            echo "        </table>
+                    
+                 </td></tr>";
+
+
+        }
+    ?>
+</table>
+
+
+
+</body>
+
+</html>
