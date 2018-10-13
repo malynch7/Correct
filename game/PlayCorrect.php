d62e525cf0bf3697ef23f8a95b5fd0f0e964f14e
Mark Lynch <malynch7@gmail.com>
2018-10-13 00:01:16 -0400

    Implemented game logic (needs styling)

--- game/PlayCorrect.php
+++ game/PlayCorrect.php
@@ -15,7 +15,7 @@
         <td>
             <ul>
                 <li><a class="active" href="../Correct.html">Home</a></li>
-                <li><a href="">Play</a></li>
+                <li><a class="active" href="">Play</a></li>
                 <li><a href="../howtoplay.html">How to play</a></li>
                 <li><a href="../youtube.html">YouTube Video</a>
                 <li><a href="../aboutcontributors.html">About Contributors</a></li>
@@ -28,8 +28,9 @@
     </tr>
     <?php
 
-        $numberOfCards = 16;
-        $gridSize = 4;
+        $gridHeight = 3;
+        $gridWidth = 4;
+        $numberOfCards = $gridHeight * $gridWidth;
 
         class Card{
 
@@ -41,8 +42,8 @@
             function __construct($face,$match){
                 $this->face = $face;
                 $this->match = $match;
-                $this->flipped = 1;
-                $this->matched = 0;
+                $this->flipped = false;
+                $this->matched = false;
             }
             function toString(){
                 return $this->face . $this->match . $this->flipped . $this->matched ;
@@ -60,17 +61,18 @@
                 }else{
                     $letter = "B";
                 }
-                array_push($deck, new Card("../images/GameDesign/card" . intdiv(($i *4) ,2) . $letter .
+                array_push($deck, new Card("../images/GameDesign/card" . intdiv($i ,2) . $letter .
                     ".png", intdiv($i , 2)));
             }
-            //shuffle($deck);
+            shuffle($deck);
 
             //present new game button
             echo "<tr><td>
                     <br><br>
                     <form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
                         <input type=\"hidden\" name=\"cards\" value=\"" . htmlspecialchars(serialize($deck)) . "\">
-                        <input type=\"hidden\" name=\"start\" value=\"true\">
+                        <input type='hidden' name='firstFlipped' value='-1'>
+                        <input type='hidden' name='cardFlipped' value='-1'>
                         <input type=\"submit\" value=\"Play Now!\">
                     </form>
                    </td></tr>
@@ -80,30 +82,92 @@
 
             //gameplay loop
             $deck = unserialize($_REQUEST["cards"]);
+            $matchMade = 0;
+            $firstFlipped = $_REQUEST["firstFlipped"];
+            //check for match
+            if($_REQUEST["cardFlipped"] != -1){
+                if($firstFlipped == -1){
+                    $firstFlipped = $_REQUEST["cardFlipped"];
+                    $deck[$firstFlipped]->flipped = true;
+                }elseif($deck[$firstFlipped]->match == $deck[$_REQUEST["cardFlipped"]]->match){
+                    $matchMade = 1;
+                    $deck[$firstFlipped]->matched = true;
+                    $deck[$_REQUEST["cardFlipped"]]->matched = true;
+                    $deck[$_REQUEST["cardFlipped"]]->flipped = true;
+                }else{
+                    $matchMade = 2;
+                    $deck[$_REQUEST["cardFlipped"]]->flipped = true;
+                }
 
+            }
+            $score = 0;
+            foreach($deck as $card){
+                if($card->matched == true){
+                    $score++;
+                }
+            }
+
+            //display cards
             echo "<tr><td>
                     <br><br>
                         <table class='center'>";
-            for($i = 0; $i < $gridSize; $i++){
+            for($i = 0; $i < $gridHeight; $i++){
                 echo "<tr class='center'>";
-                for($j = 0; $j < $gridSize; $j++){
-                    if($deck[($i + $j)]->flipped == 1){
-                        echo "<td><img src='" . $deck[(($i * 4) + $j)]->face ."' class='center'></td>";
+                for($j = 0; $j < $gridWidth; $j++){
+                    if($deck[($i * $gridWidth) + $j]->flipped == true){
+                        echo "<td><img src='" . $deck[(($i * $gridWidth) + $j)]->face ."' class='center'></td>";
+                    }elseif($matchMade == 0){
+                        echo "<td><form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
+                                <input type=\"hidden\" name=\"cards\" value=\"" . htmlspecialchars(serialize($deck)) . "\">
+                                <input type='hidden' name='cardFlipped' value='" . (($i * $gridWidth) + $j) . "'>
+                                <input type='hidden' name='firstFlipped' value='" . $firstFlipped . "'>
+                                <input type='image' alt=\"submit\" src='../images/GameDesign/backside_of_card.png'>
+                             </form></td>";
                     }else{
                         echo "<td><img src='../images/GameDesign/backside_of_card.png' class='center'></td>";
                     }
-
                 }
-                echo "</tr>";
+                echo"</tr>";
             }
-            echo "        </table>
-                    
-                 </td></tr>";
+            echo "</table></td>";
 
+           //win condition
+            if($score == $numberOfCards){
+                echo "<td><br><br>
+                      <p class='center'>You Have Won!</p>
+                      <br>
+                      <form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
+                                <input type='submit' value='Play Again'>
+                             </form></td>";
+            //Match made
+            }elseif($matchMade == 1){
+                echo "<td><br><br>
+                      <p class='center'>You Found a Match!</p>
+                      <br>
+                      <form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
+                                <input type=\"hidden\" name=\"cards\" value=\"" . htmlspecialchars(serialize($deck)) . "\">
+                                <input type='hidden' name='firstFlipped' value='-1'>
+                                <input type='hidden' name='cardFlipped' value='-1'>
+                                <input type='submit' value='Continue'>
+                             </form></td>";
 
+            //match failed
+            }elseif($matchMade == 2){
+                $deck[$_REQUEST["cardFlipped"]]->flipped = false;
+                $deck[$firstFlipped]->flipped = false;
+                echo "<td><br><br>
+                      <p class='center'>These Do Not Match</p>
+                      <br>
+                      <form class=\"center\"method=\"post\" action=\"" . htmlspecialchars($_SERVER[PHP_SELF]) . "\">
+                                <input type=\"hidden\" name=\"cards\" value=\"" . htmlspecialchars(serialize($deck)) . "\">
+                                <input type='hidden' name='firstFlipped' value='-1'>
+                                <input type='hidden' name='cardFlipped' value='-1'>
+                                <input type='submit' value='Continue'>
+                             </form></td>";
+            }
         }
     ?>
-</table>
+    </tr></table>
 
 
 
